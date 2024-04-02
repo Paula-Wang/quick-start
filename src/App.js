58600ebde97b1575dea874322cf6e89b3af12c94
Paula-Wang <nimopaula8@gmail.com>
2024-04-02 14:23:09 +0300

    feat: :sparkles: add code for Writing markup with JSX

--- src/App.js
+++ src/App.js
@@ -6,11 +6,34 @@ function MyButton() {
   );
 }
 
-export default function MyApp() {
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//     </div>
+//   );
+// } 
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
   );
-} 
+}
