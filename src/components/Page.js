657c1bafaa072bfb2b3af01101196f538aafbf2a
Paula-Wang <nimopaula8@gmail.com>
2024-04-02 20:02:50 +0300

    feat: :sparkles: change code to suit the example for Using and providing context from the same component

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,24 +3,23 @@ import Section from "./Section";
 
 export default function Page() {
     return (
-        <Section level={1}>
-        <Heading>Title</Heading>
-        <Section level={2}>
-          <Heading>Heading</Heading>
-          <Heading>Heading</Heading>
-          <Heading>Heading</Heading>
-          <Section level={3}>
-            <Heading>Sub-heading</Heading>
-            <Heading>Sub-heading</Heading>
-            <Heading>Sub-heading</Heading>
-            <Section level={4}>
-              <Heading>Sub-sub-heading</Heading>
-              <Heading>Sub-sub-heading</Heading>
-              <Heading>Sub-sub-heading</Heading>
-            </Section>
+        <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
           </Section>
         </Section>
       </Section>
-    );
-  }
-  
+    </Section>
+  );
+}
