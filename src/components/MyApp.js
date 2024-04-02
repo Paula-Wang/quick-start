19c782804435fdce08e9199ed95195c6b6fb166c
Paula-Wang <nimopaula8@gmail.com>
2024-04-02 14:10:08 +0300

    feat: :sparkles: add MyButton, MyApp for Creating and nesting components section

---
+++ src/components/MyApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
