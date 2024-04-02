f8d93ad3257b8045f43fe6272e047349026e95cd
Paula-Wang <nimopaula8@gmail.com>
2024-04-02 16:06:25 +0300

    feat: :sparkles: add code for Responding to events section

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {useState} from 'react';
 
 function MyButton() {
+    const [count, setCount] = useState(0);
+
+    function handleClick () {
+        setCount(count + 1);
+        alert('You clicked me!');
+    }
     return (
-        <button>I'm a button</button>
+        <button onClick={handleClick}>I'm a button. Click me</button>
     );
 }
 
