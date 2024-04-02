f77adea61cc5fe17196284cf26497471930bae8f
Paula-Wang <nimopaula8@gmail.com>
2024-04-02 15:53:05 +0300

    feat: :sparkles: add ShoppingList for Conditional rendering section
    add for rendering lists subsection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./App.css";
 import MyApp from './components/MyApp';
 import Profile from './components/Profile';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div>
       <MyApp />
       <Profile />
+      <ShoppingList />
     </div>
   );
 }
